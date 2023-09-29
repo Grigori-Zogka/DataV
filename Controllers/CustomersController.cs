93702a760183724fa76e0c9605fbd38692f421c1
ARTEMIS-ITS\grigoris.zogkas <grigoris.zogkas@artemis-its.com>
2023-09-29 15:05:57 +0300

    UI Changes +  botstrap modal to all acions

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -23,6 +23,7 @@ namespace DataV.Controllers
         public async Task<IActionResult> Index()
         {
             return _context.Customers != null ?
+                
                        View(await _context.Customers.ToListAsync()) :
                         Problem("Entity set 'CustomerContext.Customers'  is null.");
 
@@ -54,8 +55,6 @@ namespace DataV.Controllers
         }
 
         // POST: Customers/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name,Age,Address,PhoneNumber,Email")] Customer customer)
@@ -86,8 +85,6 @@ namespace DataV.Controllers
         }
 
         // POST: Customers/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Age,Address,PhoneNumber,Email")] Customer customer)
